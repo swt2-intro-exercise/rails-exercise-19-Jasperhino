ffaf7eff2bd5388f3ae061c49c6cc55b775687a5
Jasper <blum.jasper@student.hpi.de>
2019-11-06 20:41:38 +0100

    failing test for #3

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
-    it "should have attributes first_name, last_name, homepage" do
-        author = Author.new("Alan", "Turing", "http://wikipedia.de/Alan_Turing")
-        expect(author.first_name).to eq("Alan")
-        expect(author.last_name).to eq("Turing")
-        expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
-    end
+
+  it "should use some attributes" do
+    author = Author.new( :first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing" )
+    expect(author.first_name).to eq("Alan")
+    expect(author.last_name).to eq("Turing")
+    expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
+  end
+
+  #it "should have name method returning 'first_name last_name'" do
+  #  expect(author.name).to eq("Alan Turing")  
+  #end
 end
