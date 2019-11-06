4162757f43b7c4d43ed08be9f610ee929d921269
Jasper <blum.jasper@student.hpi.de>
2019-11-06 20:45:56 +0100

    add test for name fuction

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  author = Author.new( :first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing" )
 
   it "should use some attributes" do
-    author = Author.new( :first_name => "Alan", :last_name => "Turing", :homepage => "http://wikipedia.org/Alan_Turing" )
     expect(author.first_name).to eq("Alan")
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
   end
 
-  #it "should have name method returning 'first_name last_name'" do
-  #  expect(author.name).to eq("Alan Turing")  
-  #end
+  it "should have name method returning 'first_name last_name'" do
+    expect(author.name).to eq("Alan Turing")  
+  end
 end
