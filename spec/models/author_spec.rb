f49d7c7ea51f6f1992f9a8b4ee1c75fb75623a85
Jasper <blum.jasper@student.hpi.de>
2019-11-06 19:54:04 +0100

    update author model test

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,8 +1,10 @@
-describe "Author Attributes", type: :model do
-    it "should have Attributes first_name, last_name, website" do
-        author = new Author("Alan", "Turing", "http://wikipedia.de/Alan_Turing")
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+    it "should have attributes first_name, last_name, homepage" do
+        author = Author.new("Alan", "Turing", "http://wikipedia.de/Alan_Turing")
         expect(author.first_name).to eq("Alan")
         expect(author.last_name).to eq("Turing")
-        expect(author.website).to eq("http://wikipedia.de/Alan_Turing")
+        expect(author.homepage).to eq("http://wikipedia.de/Alan_Turing")
     end
 end
