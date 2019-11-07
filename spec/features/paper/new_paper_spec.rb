c65713205f56a0e039ab89f1e3713bd2a6f35d10
Jasper <blum.jasper@student.hpi.de>
2019-11-07 11:04:30 +0100

    add failing test #15

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -6,14 +6,4 @@ describe "New paper page", type: :feature do
     visit new_paper_path
   end
 
-  it "should fail validation without all attributes" do
-    paper = Paper.new()
-    expect(paper).to_not be_valid 
-  end
-
-  it "should fail validation with string in year attribute" do
-    paper = Paper.new(:title => 'HPI', :venue => 'Potsdam', :year => 'nineteen-fifty')
-    expect(paper).to_not be_valid 
-  end
-
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  paper = Paper.new(:title => 'HPI', :venue => 'Potsdam', :year => 1999)
+
+  it "should fail validation without all attributes" do
+    paper = Paper.new()
+    expect(paper).to_not be_valid 
+  end
+
+  it "should fail validation with string in year attribute" do
+    paper = Paper.new(:title => 'HPI', :venue => 'Potsdam', :year => 'nineteen-fifty')
+    expect(paper).to_not be_valid 
+  end
+
+  it "should have empty authors list" do
+    expect(paper.authors.count).to eq(0)  
+  end
 end
