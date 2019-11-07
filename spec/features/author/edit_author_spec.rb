9c93841807201153fb07e94c22ebb22853c174b4
Jasper <blum.jasper@student.hpi.de>
2019-11-07 08:56:49 +0100

    add test for #9

--- spec/features/author/edit_author_spec.rb
+++ spec/features/author/edit_author_spec.rb
@@ -1,7 +1,25 @@
 require 'rails_helper'
 
+before(:each) do
+    @alan = FactoryBot.create :author
+  end
+
 describe "New author page", type: :feature do
   it "should render withour error" do
-    visit edit_author_path
+    visit edit_author_path(@alan)
+  end
+
+  it "should edit author" do
+    visit edit_author_path(@alan)
+
+    page.fill_in 'author[first_name]', with: @alan.first_name
+    page.fill_in 'author[last_name]', with: @alan.first_name
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.de/Edit'
+
+    find('input[type="submit"]').click
+
+    @alan.reload
+
+    expect(@alan.homepage).to be 'http://wikipedia.de/Edit'
   end
 end
