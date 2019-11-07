b7b1ff5d82b75f3683f911e8ee7596fffe2d9a51
Jasper <blum.jasper@student.hpi.de>
2019-11-07 10:50:59 +0100

    add failing test #14

--- spec/features/paper/new_paper_spec.rb
+++ spec/features/paper/new_paper_spec.rb
@@ -6,8 +6,13 @@ describe "New paper page", type: :feature do
     visit new_paper_path
   end
 
-  it "should fail validation without all atributes" do
+  it "should fail validation without all attributes" do
     paper = Paper.new()
+    expect(paper).to_not be_valid 
+  end
+
+  it "should fail validation with string in year attribute" do
+    paper = Paper.new(:title => 'HPI', :venue => 'Potsdam', :year => 'nineteen-fifty')
     expect(paper).to_not be_valid 
   end
 
