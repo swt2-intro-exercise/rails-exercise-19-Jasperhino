6084c0f16e9ff249949b568a8d414d0f917e4dca
Jasper <blum.jasper@student.hpi.de>
2019-11-07 13:08:00 +0100

    update test #17

--- spec/features/paper/edit_paper_spec.rb
+++ spec/features/paper/edit_paper_spec.rb
@@ -4,7 +4,6 @@ require 'rails_helper'
 describe "Edit paper page", type: :feature do
 
   before(:each) do
-    @alan = FactoryBot.create :author
     @computing = FactoryBot.create :paper
   end
 
@@ -15,10 +14,17 @@ describe "Edit paper page", type: :feature do
   it "should allow to select paper authors from a multiple select box" do
     visit edit_paper_path(@computing)
     expect(@computing.authors.count).to eq(1)
-    page.unselect @alan, from: 'author_ids'
+    page.unselect 'Alan Turing', from: 'paper[author_ids][]', match: :first
     find('input[type="submit"]').click
     @computing.reload
     expect(@computing.authors.count).to eq(0)
+
+    visit edit_paper_path(@computing)
+    expect(@computing.authors.count).to eq(0)
+    page.select 'Alan Turing', from: 'paper[author_ids][]', match: :first
+    find('input[type="submit"]').click
+    @computing.reload
+    expect(@computing.authors.count).to eq(1)
   end
 
 end
