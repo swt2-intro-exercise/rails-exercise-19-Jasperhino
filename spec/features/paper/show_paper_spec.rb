21e4f22e019271085aa76dc3920d18d75846783a
Jasper <blum.jasper@student.hpi.de>
2019-11-07 11:19:01 +0100

    add failing test #16

---
+++ spec/features/paper/show_paper_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+describe "Show paper page", type: :feature do
+    before(:each) do
+        @alan = FactoryBot.create :author
+        @computing = FactoryBot.create :paper
+    end
+
+  it "should render withour error" do
+    visit show_paper_path(@computing)
+  end
+
+  it "should display authors name" do
+    visit show_paper_path(@computing)
+
+    expect(page).to have_text(@alan.name)
+  end
+
+end
